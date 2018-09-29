45932cdee8d6e441d32817c4b326d5ac5d082491
arkrstm <arkrstm@gmail.com>
2018-09-29 13:58:54 +0300

    second

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python mbot.py
+web: python mbotF.py
---
+++ mbotF.py
@@ -0,0 +1,37 @@
+import telebot
+import cherrypy
+import os
+
+token = '660103023:AAEUiNb-8--UN82OhK7r_8erdy4tsBKC6cM'
+WEBHOOK_HOST = 'cryptic-citadel-53949.herokuapp.com'
+WEBHOOK_PORT = 8443#8443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (token)
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)			
+	
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://cryptic-citadel-53949.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
