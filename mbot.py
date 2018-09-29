4ec927bdc53506961dc0293cd0d7f8ade91bce78
arkrstm <arkrstm@gmail.com>
2018-09-29 12:36:35 +0300

    second

--- mbot.py
+++ mbot.py
@@ -2,7 +2,7 @@ import telebot
 import cherrypy
 
 token = '660103023:AAEUiNb-8--UN82OhK7r_8erdy4tsBKC6cM'
-WEBHOOK_HOST = 'https://cryptic-citadel-53949.herokuapp.com/'
+WEBHOOK_HOST = 'cryptic-citadel-53949.herokuapp.com/'
 WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
 WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
 
