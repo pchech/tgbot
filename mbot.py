fce2247d0cb2eb664a42150eb6826658fca5086c
arkrstm <arkrstm@gmail.com>
2018-09-29 13:01:38 +0300

    second

--- mbot.py
+++ mbot.py
@@ -3,7 +3,7 @@ import cherrypy
 
 token = '660103023:AAEUiNb-8--UN82OhK7r_8erdy4tsBKC6cM'
 WEBHOOK_HOST = 'cryptic-citadel-53949.herokuapp.com'
-WEBHOOK_PORT = 8443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+WEBHOOK_PORT = 88  # 443, 80, 88 или 8443 (порт должен быть открыт!)
 WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
