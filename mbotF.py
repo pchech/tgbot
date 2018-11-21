8304af2fdfe7382d9523260a83d6921aebe025a4
pchech <40461901+pchech@users.noreply.github.com>
2018-11-21 18:37:26 +0300

    modules

--- mbotF.py
+++ mbotF.py
@@ -47,9 +47,8 @@ def check_photo(message):
 	file = bot.get_file(photo)
 	bot.send_message(message.chat.id,photo)
 	bot.send_message(message.chat.id,file.file_path)
-	url = 'https://api.telegram.org/file/bot{}/{}'.format(token,file.file_path)
-	bot.send_message(message.chat.id, url)
-	bot.send_photo(message.chat.id, url)
+	downloaded_file = bot.download_file(file.file_path)
+	bot.send_photo(message.chat.id, downloaded_file)
 	
 @bot.message_handler(func=is_normal, content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
